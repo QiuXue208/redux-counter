6d916c8cc4facb7d6f099c037f372b4602effeb5
Snowy <1309571292@qq.com>
2018-12-03 16:57:28 +0800

    remove build

---
+++ build/precache-manifest.0ffdeb98288fba0e96c089f8019375c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "873ec2e363c7eb11e8ec",
+    "url": "/static/js/main.873ec2e3.chunk.js"
+  },
+  {
+    "revision": "50c61b12ee88189797f2",
+    "url": "/static/js/1.50c61b12.chunk.js"
+  },
+  {
+    "revision": "873ec2e363c7eb11e8ec",
+    "url": "/static/css/main.5b381557.chunk.css"
+  },
+  {
+    "revision": "9b9b0cfd05008ce040edd307537c7b43",
+    "url": "/index.html"
+  }
+];
